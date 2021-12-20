c9a0ed575132a7fe4aae178b45d1bf00ad16de88
yuuki.oikawa2 <yuki.oikawa.g@gmail.com>
2021-12-20 12:24:44 +0000

    new kadai-ruby-opp-2 commit

---
+++ mentor.rb
@@ -0,0 +1,36 @@
+class Mentor
+    # インスタンス変数
+    attr_accessor :name 
+
+    def initialize(name)
+        self.name = name
+    end
+ 
+    def job
+        puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+    end
+    
+end
+
+class RailsMentor < Mentor
+    # インスタンス変数
+    attr_accessor :name 
+    
+    def initialize(name)
+        self.name = name
+    end
+    
+    def job 
+        puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+    end
+end    
+
+#インスタンスの生成
+kirameki=Mentor.new('煌木')
+akaide = RailsMentor.new('赤出')
+
+
+#jobの呼び出し
+kirameki.job
+akaide.job
+
